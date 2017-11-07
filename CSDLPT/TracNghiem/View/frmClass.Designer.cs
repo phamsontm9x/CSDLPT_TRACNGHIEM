bdbff2378ca99b0779f801fc6e085d0ea87f319f
HHumorous <hhoangna@gmail.com>
2017-11-07 13:06:14 +0700

    Init Class Form

--- CSDLPT/TracNghiem/View/frmClass.Designer.cs
+++ CSDLPT/TracNghiem/View/frmClass.Designer.cs
@@ -28,19 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmClass));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbbDep = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataSetTracNghiem = new TracNghiem.dataSetTracNghiem();
+            this.bdsClass = new System.Windows.Forms.BindingSource(this.components);
+            this.lOPTableAdapter = new TracNghiem.dataSetTracNghiemTableAdapters.LOPTableAdapter();
+            this.tableAdapterManager = new TracNghiem.dataSetTracNghiemTableAdapters.TableAdapterManager();
+            this.lOPGridControl = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.cbbBranch = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetTracNghiem)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsClass)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lOPGridControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.cbbBranch);
+            this.groupBox1.Controls.Add(this.cbbDep);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(1, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1028, 269);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // cbbDep
+            // 
+            this.cbbDep.FormattingEnabled = true;
+            this.cbbDep.Location = new System.Drawing.Point(101, 118);
+            this.cbbDep.Name = "cbbDep";
+            this.cbbDep.Size = new System.Drawing.Size(325, 30);
+            this.cbbDep.TabIndex = 3;
+            this.cbbDep.SelectedIndexChanged += new System.EventHandler(this.cbbDep_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("VNI-Jamai", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 55);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Class Info";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Location = new System.Drawing.Point(0, 275);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1028, 257);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // dataSetTracNghiem
+            // 
+            this.dataSetTracNghiem.DataSetName = "dataSetTracNghiem";
+            this.dataSetTracNghiem.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bdsClass
+            // 
+            this.bdsClass.DataMember = "LOP";
+            this.bdsClass.DataSource = this.dataSetTracNghiem;
+            // 
+            // lOPTableAdapter
+            // 
+            this.lOPTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.BAITHITableAdapter = null;
+            this.tableAdapterManager.BANGDIEMTableAdapter = null;
+            this.tableAdapterManager.BODETableAdapter = null;
+            this.tableAdapterManager.COSOTableAdapter = null;
+            this.tableAdapterManager.GIAOVIEN_DANGKYTableAdapter = null;
+            this.tableAdapterManager.GIAOVIENTableAdapter = null;
+            this.tableAdapterManager.KHOATableAdapter = null;
+            this.tableAdapterManager.LOPTableAdapter = this.lOPTableAdapter;
+            this.tableAdapterManager.MONHOCTableAdapter = null;
+            this.tableAdapterManager.SINHVIENTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = TracNghiem.dataSetTracNghiemTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // lOPGridControl
+            // 
+            this.lOPGridControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lOPGridControl.DataSource = this.bdsClass;
+            this.lOPGridControl.Location = new System.Drawing.Point(7, 316);
+            this.lOPGridControl.MainView = this.gridView1;
+            this.lOPGridControl.Name = "lOPGridControl";
+            this.lOPGridControl.Size = new System.Drawing.Size(1022, 244);
+            this.lOPGridControl.TabIndex = 2;
+            this.lOPGridControl.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.lOPGridControl;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsDetail.DetailMode = DevExpress.XtraGrid.Views.Grid.DetailMode.Default;
+            this.gridView1.OptionsView.ShowGroupPanel = false;
+            // 
+            // cbbBranch
+            // 
+            this.cbbBranch.FormattingEnabled = true;
+            this.cbbBranch.Location = new System.Drawing.Point(101, 177);
+            this.cbbBranch.Name = "cbbBranch";
+            this.cbbBranch.Size = new System.Drawing.Size(325, 30);
+            this.cbbBranch.TabIndex = 4;
+            this.cbbBranch.SelectedIndexChanged += new System.EventHandler(this.cbbBranch_SelectedIndexChanged);
             // 
             // frmClass
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(975, 574);
+            this.ClientSize = new System.Drawing.Size(1030, 572);
+            this.Controls.Add(this.lOPGridControl);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmClass";
-            this.Text = "frmClass";
+            this.Text = "Class";
+            this.Load += new System.EventHandler(this.frmClass_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetTracNghiem)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsClass)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lOPGridControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbbDep;
+        private dataSetTracNghiem dataSetTracNghiem;
+        private System.Windows.Forms.BindingSource bdsClass;
+        private dataSetTracNghiemTableAdapters.LOPTableAdapter lOPTableAdapter;
+        private dataSetTracNghiemTableAdapters.TableAdapterManager tableAdapterManager;
+        private DevExpress.XtraGrid.GridControl lOPGridControl;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private System.Windows.Forms.ComboBox cbbBranch;
     }
 }
--- CSDLPT/TracNghiem/View/frmClass.cs
+++ CSDLPT/TracNghiem/View/frmClass.cs
@@ -9,12 +9,71 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TracNghiem
-{
+{   
     public partial class frmClass : Form
     {
+        int index;
+        String depID = "";
         public frmClass()
         {
             InitializeComponent();
+        }
+
+        private void cbbDep_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbbDep.SelectedValue != null)
+            {
+                if (cbbDep.SelectedValue.ToString() == "System.Data.DataRowView") return;
+                Program.serverName = cbbDep.SelectedValue.ToString();
+
+                if (cbbDep.SelectedIndex != Program.currentBranch)
+                {
+                    Program.userName = Program.remoteLogin;
+                    Program.password = Program.remotePass;
+                }
+                else
+                {
+                    Program.userName = Program.currentUserName;
+                    Program.password = Program.currentPass;
+                }
+                if (Program.Connection() == 0)
+                    MessageBox.Show("Lỗi kết nối về chi nhánh mới", "", MessageBoxButtons.OK);
+                else
+                {
+                    this.lOPTableAdapter.Connection.ConnectionString = Program.connectStr;
+                    this.lOPTableAdapter.Fill(this.dataSetTracNghiem.LOP);
+                    depID = ((DataRowView)bdsClass[0])["MAKH"].ToString();
+                }
+            }
+        }
+
+        private void lOPBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.bdsClass.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dataSetTracNghiem);
+
+        }
+
+        private void frmClass_Load(object sender, EventArgs e)
+        {
+            dataSetTracNghiem.EnforceConstraints = false;
+            this.lOPTableAdapter.Connection.ConnectionString = Program.connectStr;
+            // TODO: This line of code loads data into the 'dataSetTracNghiem.LOP' table. You can move, or remove it, as needed.
+            this.lOPTableAdapter.Fill(this.dataSetTracNghiem.LOP);
+            depID = ((DataRowView)bdsClass[0])["MAKH"].ToString();
+            cbbDep.DataSource = Program.bds;
+            cbbDep.DisplayMember = "MACS";
+            cbbDep.ValueMember = "TENCS";
+            cbbDep.SelectedIndex = Program.currentBranch;
+
+            if (Program.currentRole == "TRUONG") cbbDep.Enabled = true;
+            else cbbDep.Enabled = false;
+        }
+
+        private void cbbBranch_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
