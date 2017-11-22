f022be5f96a50ed6399ccf07d734e2b8f5bd5f21
HHumorous <hhoangna@gmail.com>
2017-11-22 11:19:17 +0700

    Registration Form

--- CSDLPT/TracNghiem/View/frmRegistration.Designer.cs
+++ CSDLPT/TracNghiem/View/frmRegistration.Designer.cs
@@ -29,10 +29,245 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRegistration));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbbSubject = new System.Windows.Forms.ComboBox();
+            this.bdsSubjects = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSetTracNghiem = new TracNghiem.dataSetTracNghiem();
+            this.barManager1 = new DevExpress.XtraBars.BarManager(this.components);
+            this.bar1 = new DevExpress.XtraBars.Bar();
+            this.btnNew = new DevExpress.XtraBars.BarButtonItem();
+            this.btnEdit = new DevExpress.XtraBars.BarButtonItem();
+            this.btnDel = new DevExpress.XtraBars.BarButtonItem();
+            this.btnSave = new DevExpress.XtraBars.BarButtonItem();
+            this.btnRefresh = new DevExpress.XtraBars.BarButtonItem();
+            this.btnCancel = new DevExpress.XtraBars.BarButtonItem();
+            this.btnClose = new DevExpress.XtraBars.BarButtonItem();
+            this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
+            this.mONHOCTableAdapter = new TracNghiem.dataSetTracNghiemTableAdapters.MONHOCTableAdapter();
+            this.cbbClass = new System.Windows.Forms.ComboBox();
+            this.cbbDep = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsSubjects)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetTracNghiem)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbbDep);
+            this.groupBox1.Controls.Add(this.cbbClass);
+            this.groupBox1.Controls.Add(this.cbbSubject);
+            this.groupBox1.Location = new System.Drawing.Point(3, 65);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1163, 280);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // cbbSubject
+            // 
+            this.cbbSubject.DataSource = this.bdsSubjects;
+            this.cbbSubject.DisplayMember = "TENMH";
+            this.cbbSubject.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbbSubject.FormattingEnabled = true;
+            this.cbbSubject.Location = new System.Drawing.Point(377, 22);
+            this.cbbSubject.Name = "cbbSubject";
+            this.cbbSubject.Size = new System.Drawing.Size(187, 24);
+            this.cbbSubject.TabIndex = 0;
+            this.cbbSubject.ValueMember = "MAMH";
+            // 
+            // bdsSubjects
+            // 
+            this.bdsSubjects.DataMember = "MONHOC";
+            this.bdsSubjects.DataSource = this.dataSetTracNghiem;
+            // 
+            // dataSetTracNghiem
+            // 
+            this.dataSetTracNghiem.DataSetName = "dataSetTracNghiem";
+            this.dataSetTracNghiem.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // barManager1
+            // 
+            this.barManager1.Bars.AddRange(new DevExpress.XtraBars.Bar[] {
+            this.bar1});
+            this.barManager1.DockControls.Add(this.barDockControlTop);
+            this.barManager1.DockControls.Add(this.barDockControlBottom);
+            this.barManager1.DockControls.Add(this.barDockControlLeft);
+            this.barManager1.DockControls.Add(this.barDockControlRight);
+            this.barManager1.Form = this;
+            this.barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.btnNew,
+            this.btnEdit,
+            this.btnDel,
+            this.btnSave,
+            this.btnRefresh,
+            this.btnCancel,
+            this.btnClose});
+            this.barManager1.MaxItemId = 7;
+            // 
+            // bar1
+            // 
+            this.bar1.BarName = "Tools";
+            this.bar1.DockCol = 0;
+            this.bar1.DockRow = 0;
+            this.bar1.DockStyle = DevExpress.XtraBars.BarDockStyle.Top;
+            this.bar1.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, this.btnNew, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph),
+            new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, this.btnEdit, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph),
+            new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, this.btnDel, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph),
+            new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, this.btnSave, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph),
+            new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, this.btnRefresh, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph),
+            new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, this.btnCancel, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph),
+            new DevExpress.XtraBars.LinkPersistInfo(DevExpress.XtraBars.BarLinkUserDefines.PaintStyle, this.btnClose, "", true, true, true, 0, null, DevExpress.XtraBars.BarItemPaintStyle.CaptionGlyph)});
+            this.bar1.Text = "Tools";
+            // 
+            // btnNew
+            // 
+            this.btnNew.Caption = "New";
+            this.btnNew.Glyph = global::TracNghiem.Properties.Resources.icons8_plus;
+            this.btnNew.Id = 0;
+            this.btnNew.LargeGlyph = global::TracNghiem.Properties.Resources.icons8_plus;
+            this.btnNew.Name = "btnNew";
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Caption = "Edit";
+            this.btnEdit.Glyph = global::TracNghiem.Properties.Resources.icons8_pencil;
+            this.btnEdit.Id = 1;
+            this.btnEdit.Name = "btnEdit";
+            // 
+            // btnDel
+            // 
+            this.btnDel.Caption = "Delete";
+            this.btnDel.Glyph = global::TracNghiem.Properties.Resources.icons8_trash;
+            this.btnDel.Id = 2;
+            this.btnDel.Name = "btnDel";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Caption = "Save";
+            this.btnSave.Glyph = global::TracNghiem.Properties.Resources.icons8_save;
+            this.btnSave.Id = 3;
+            this.btnSave.Name = "btnSave";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Caption = "Refresh";
+            this.btnRefresh.Glyph = global::TracNghiem.Properties.Resources.icons8_refresh;
+            this.btnRefresh.Id = 4;
+            this.btnRefresh.Name = "btnRefresh";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Caption = "Cancel";
+            this.btnCancel.Glyph = global::TracNghiem.Properties.Resources.icons8_cancel;
+            this.btnCancel.Id = 5;
+            this.btnCancel.Name = "btnCancel";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Caption = "Close";
+            this.btnClose.Glyph = global::TracNghiem.Properties.Resources.icons8_home;
+            this.btnClose.Id = 6;
+            this.btnClose.Name = "btnClose";
+            // 
+            // barDockControlTop
+            // 
+            this.barDockControlTop.CausesValidation = false;
+            this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlTop.Size = new System.Drawing.Size(1166, 59);
+            // 
+            // barDockControlBottom
+            // 
+            this.barDockControlBottom.CausesValidation = false;
+            this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.barDockControlBottom.Location = new System.Drawing.Point(0, 659);
+            this.barDockControlBottom.Size = new System.Drawing.Size(1166, 0);
+            // 
+            // barDockControlLeft
+            // 
+            this.barDockControlLeft.CausesValidation = false;
+            this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.barDockControlLeft.Location = new System.Drawing.Point(0, 59);
+            this.barDockControlLeft.Size = new System.Drawing.Size(0, 600);
+            // 
+            // barDockControlRight
+            // 
+            this.barDockControlRight.CausesValidation = false;
+            this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.barDockControlRight.Location = new System.Drawing.Point(1166, 59);
+            this.barDockControlRight.Size = new System.Drawing.Size(0, 600);
+            // 
+            // mONHOCTableAdapter
+            // 
+            this.mONHOCTableAdapter.ClearBeforeFill = true;
+            // 
+            // cbbClass
+            // 
+            this.cbbClass.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbbClass.FormattingEnabled = true;
+            this.cbbClass.Location = new System.Drawing.Point(627, 22);
+            this.cbbClass.Name = "cbbClass";
+            this.cbbClass.Size = new System.Drawing.Size(187, 24);
+            this.cbbClass.TabIndex = 1;
+            // 
+            // cbbDep
+            // 
+            this.cbbDep.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbbDep.FormattingEnabled = true;
+            this.cbbDep.Location = new System.Drawing.Point(133, 22);
+            this.cbbDep.Name = "cbbDep";
+            this.cbbDep.Size = new System.Drawing.Size(187, 24);
+            this.cbbDep.TabIndex = 2;
+            // 
+            // frmRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1166, 659);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.barDockControlLeft);
+            this.Controls.Add(this.barDockControlRight);
+            this.Controls.Add(this.barDockControlBottom);
+            this.Controls.Add(this.barDockControlTop);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmRegistration";
             this.Text = "frmRegistration";
+            this.Load += new System.EventHandler(this.frmRegistration_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bdsSubjects)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetTracNghiem)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private DevExpress.XtraBars.BarManager barManager1;
+        private DevExpress.XtraBars.Bar bar1;
+        private DevExpress.XtraBars.BarButtonItem btnNew;
+        private DevExpress.XtraBars.BarButtonItem btnEdit;
+        private DevExpress.XtraBars.BarButtonItem btnDel;
+        private DevExpress.XtraBars.BarButtonItem btnSave;
+        private DevExpress.XtraBars.BarButtonItem btnRefresh;
+        private DevExpress.XtraBars.BarButtonItem btnCancel;
+        private DevExpress.XtraBars.BarButtonItem btnClose;
+        private DevExpress.XtraBars.BarDockControl barDockControlTop;
+        private DevExpress.XtraBars.BarDockControl barDockControlBottom;
+        private DevExpress.XtraBars.BarDockControl barDockControlLeft;
+        private DevExpress.XtraBars.BarDockControl barDockControlRight;
+        private System.Windows.Forms.ComboBox cbbSubject;
+        private dataSetTracNghiem dataSetTracNghiem;
+        private System.Windows.Forms.BindingSource bdsSubjects;
+        private dataSetTracNghiemTableAdapters.MONHOCTableAdapter mONHOCTableAdapter;
+        private System.Windows.Forms.ComboBox cbbClass;
+        private System.Windows.Forms.ComboBox cbbDep;
     }
 }
--- CSDLPT/TracNghiem/View/frmRegistration.cs
+++ CSDLPT/TracNghiem/View/frmRegistration.cs
@@ -16,5 +16,13 @@ namespace TracNghiem
         {
             InitializeComponent();
         }
+
+        private void frmRegistration_Load(object sender, EventArgs e)
+        {
+            dataSetTracNghiem.EnforceConstraints = false;
+            // TODO: This line of code loads data into the 'dataSetTracNghiem.MONHOC' table. You can move, or remove it, as needed.
+            this.mONHOCTableAdapter.Fill(this.dataSetTracNghiem.MONHOC);
+
+        }
     }
 }
