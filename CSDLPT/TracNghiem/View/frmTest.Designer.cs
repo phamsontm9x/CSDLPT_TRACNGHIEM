ec22732937a3d1df506fa0594cbde89d961bd2e9
ThanhSon <thanhsoncun@gmail.com>
2017-11-23 16:03:59 +0700

    FrmTest

--- CSDLPT/TracNghiem/View/frmTest.Designer.cs
+++ CSDLPT/TracNghiem/View/frmTest.Designer.cs
@@ -30,12 +30,12 @@
         {
             this.bar1 = new DevExpress.XtraBars.Bar();
             this.groupQuestion = new System.Windows.Forms.GroupBox();
+            this.btnAnswer4 = new System.Windows.Forms.RadioButton();
+            this.btnAnswer3 = new System.Windows.Forms.RadioButton();
+            this.btnAnswer2 = new System.Windows.Forms.RadioButton();
+            this.btnAnswer1 = new System.Windows.Forms.RadioButton();
             this.label1 = new System.Windows.Forms.Label();
             this.lbQuestion = new System.Windows.Forms.ListBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
             this.groupQuestion.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,10 +50,10 @@
             // 
             // groupQuestion
             // 
-            this.groupQuestion.Controls.Add(this.radioButton4);
-            this.groupQuestion.Controls.Add(this.radioButton3);
-            this.groupQuestion.Controls.Add(this.radioButton2);
-            this.groupQuestion.Controls.Add(this.radioButton1);
+            this.groupQuestion.Controls.Add(this.btnAnswer4);
+            this.groupQuestion.Controls.Add(this.btnAnswer3);
+            this.groupQuestion.Controls.Add(this.btnAnswer2);
+            this.groupQuestion.Controls.Add(this.btnAnswer1);
             this.groupQuestion.Controls.Add(this.label1);
             this.groupQuestion.Location = new System.Drawing.Point(176, 83);
             this.groupQuestion.Name = "groupQuestion";
@@ -61,6 +61,54 @@
             this.groupQuestion.TabIndex = 0;
             this.groupQuestion.TabStop = false;
             this.groupQuestion.Text = "groupQuestion";
+            // 
+            // btnAnswer4
+            // 
+            this.btnAnswer4.AutoSize = true;
+            this.btnAnswer4.Location = new System.Drawing.Point(48, 244);
+            this.btnAnswer4.Name = "btnAnswer4";
+            this.btnAnswer4.Size = new System.Drawing.Size(110, 21);
+            this.btnAnswer4.TabIndex = 10;
+            this.btnAnswer4.TabStop = true;
+            this.btnAnswer4.Text = "radioButton4";
+            this.btnAnswer4.UseVisualStyleBackColor = true;
+            this.btnAnswer4.CheckedChanged += new System.EventHandler(this.btnAnswer4_CheckedChanged);
+            // 
+            // btnAnswer3
+            // 
+            this.btnAnswer3.AutoSize = true;
+            this.btnAnswer3.Location = new System.Drawing.Point(48, 198);
+            this.btnAnswer3.Name = "btnAnswer3";
+            this.btnAnswer3.Size = new System.Drawing.Size(110, 21);
+            this.btnAnswer3.TabIndex = 9;
+            this.btnAnswer3.TabStop = true;
+            this.btnAnswer3.Text = "radioButton3";
+            this.btnAnswer3.UseVisualStyleBackColor = true;
+            this.btnAnswer3.CheckedChanged += new System.EventHandler(this.btnAnswer3_CheckedChanged);
+            // 
+            // btnAnswer2
+            // 
+            this.btnAnswer2.AutoSize = true;
+            this.btnAnswer2.Location = new System.Drawing.Point(48, 158);
+            this.btnAnswer2.Name = "btnAnswer2";
+            this.btnAnswer2.Size = new System.Drawing.Size(110, 21);
+            this.btnAnswer2.TabIndex = 8;
+            this.btnAnswer2.TabStop = true;
+            this.btnAnswer2.Text = "radioButton2";
+            this.btnAnswer2.UseVisualStyleBackColor = true;
+            this.btnAnswer2.CheckedChanged += new System.EventHandler(this.btnAnswer2_CheckedChanged);
+            // 
+            // btnAnswer1
+            // 
+            this.btnAnswer1.AutoSize = true;
+            this.btnAnswer1.Location = new System.Drawing.Point(48, 113);
+            this.btnAnswer1.Name = "btnAnswer1";
+            this.btnAnswer1.Size = new System.Drawing.Size(110, 21);
+            this.btnAnswer1.TabIndex = 7;
+            this.btnAnswer1.TabStop = true;
+            this.btnAnswer1.Text = "radioButton1";
+            this.btnAnswer1.UseVisualStyleBackColor = true;
+            this.btnAnswer1.CheckedChanged += new System.EventHandler(this.btnAnswer1_CheckedChanged);
             // 
             // label1
             // 
@@ -74,56 +122,15 @@
             // 
             // lbQuestion
             // 
+            this.lbQuestion.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
             this.lbQuestion.FormattingEnabled = true;
             this.lbQuestion.ItemHeight = 16;
             this.lbQuestion.Location = new System.Drawing.Point(2, 89);
             this.lbQuestion.Name = "lbQuestion";
-            this.lbQuestion.Size = new System.Drawing.Size(168, 500);
+            this.lbQuestion.Size = new System.Drawing.Size(168, 308);
             this.lbQuestion.TabIndex = 1;
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(48, 113);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(110, 21);
-            this.radioButton1.TabIndex = 7;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "radioButton1";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(48, 158);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(110, 21);
-            this.radioButton2.TabIndex = 8;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "radioButton2";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(48, 198);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(110, 21);
-            this.radioButton3.TabIndex = 9;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "radioButton3";
-            this.radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // radioButton4
-            // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(48, 244);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(110, 21);
-            this.radioButton4.TabIndex = 10;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "radioButton4";
-            this.radioButton4.UseVisualStyleBackColor = true;
+            this.lbQuestion.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.lbQuestion_DrawItem);
+            this.lbQuestion.SelectedIndexChanged += new System.EventHandler(this.lbQuestion_SelectedIndexChanged);
             // 
             // frmTest
             // 
@@ -146,9 +153,9 @@
         private System.Windows.Forms.GroupBox groupQuestion;
         private System.Windows.Forms.ListBox lbQuestion;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RadioButton radioButton4;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton btnAnswer4;
+        private System.Windows.Forms.RadioButton btnAnswer3;
+        private System.Windows.Forms.RadioButton btnAnswer2;
+        private System.Windows.Forms.RadioButton btnAnswer1;
     }
 }
--- CSDLPT/TracNghiem/View/frmTest.cs
+++ CSDLPT/TracNghiem/View/frmTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,16 +13,146 @@ namespace TracNghiem
 {
     public partial class frmTest : Form
     {
+        public class ItemQuestion
+        {
+            public string title;
+            public int status;
+            public string answer1;
+            public string answer2;
+            public string answer3;
+            public string answer4;
+            public int answer;
+            public ItemQuestion(string Title, int Status)
+            {
+                title = Title;
+                status = Status;
+            }
+        }
+
+        public List<ItemQuestion> listQuestion;
+ 
         public frmTest()
         {
             InitializeComponent();
+            listQuestion = new List<ItemQuestion>();
             getDataQuestion();
         }
-        
 
         public void getDataQuestion()
         {
+            for (int i = 0; i < 30; i++)
+            {
+                ItemQuestion item = new ItemQuestion("Cau " + i, 0);
+                lbQuestion.Items.Add(item.title);
+                item.answer1 = "Cau tra loi A " + i;
+                item.answer2 = "Cau tra loi B " + i;
+                item.answer3 = "Cau tra loi C " + i;
+                item.answer4 = "Cau tra loi D " + i;
+                
+                listQuestion.Add(item);
+            }
+        }
 
+        public void updateDataListBox ()
+        {
+            lbQuestion.Update();
+        }
+
+
+        public void initQuestionAtIndex(int indexQuestion)
+        {
+            ItemQuestion item = listQuestion[indexQuestion];
+            btnAnswer1.Text = item.answer1;
+            btnAnswer2.Text = item.answer2;
+            btnAnswer3.Text = item.answer3;
+            btnAnswer4.Text = item.answer4;
+
+            setCheckAnswerQuestion(item.answer);
+        }
+
+        public void selectedAnswer(int index, int value)
+        {
+            ItemQuestion item = listQuestion[index];
+            item.answer = value;
+            updateDataListBox();
+        }
+
+        private void lbQuestion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            initQuestionAtIndex(lbQuestion.SelectedIndex);
+        }
+
+        private void btnAnswer1_CheckedChanged(object sender, EventArgs e)
+        {
+            int index = lbQuestion.SelectedIndex;
+            if (index >= 0)
+            {
+                selectedAnswer(index, 1);
+            }
+
+        }
+
+        private void btnAnswer2_CheckedChanged(object sender, EventArgs e)
+        {
+            int index = lbQuestion.SelectedIndex;
+            if (index >= 0)
+            {
+                selectedAnswer(index, 2);
+            }
+        }
+
+        private void btnAnswer3_CheckedChanged(object sender, EventArgs e)
+        {
+            int index = lbQuestion.SelectedIndex;
+            if (index >= 0)
+            {
+                selectedAnswer(index, 3);
+            }
+        }
+
+        private void btnAnswer4_CheckedChanged(object sender, EventArgs e)
+        {
+            int index = lbQuestion.SelectedIndex;
+            if (index >= 0)
+            {
+                selectedAnswer(index, 4);
+            }
+        }
+
+        public void setCheckAnswerQuestion (int index)
+        {
+            if (index == 1)
+            {
+                btnAnswer1.Checked = true;
+            }
+            else if (index == 2)
+            {
+                btnAnswer2.Checked = true;
+            }
+            else if (index == 3)
+            {
+                btnAnswer3.Checked = true;
+            }
+            else if (index == 4)
+            {
+                btnAnswer4.Checked = true;
+            }
+            else
+            {
+                btnAnswer1.Checked = false;
+                btnAnswer2.Checked = false;
+                btnAnswer3.Checked = false;
+                btnAnswer4.Checked = false;
+            }
+        }
+
+        private void lbQuestion_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            ItemQuestion item = listQuestion[e.Index];
+            e.DrawBackground();
+            Graphics g = e.Graphics;
+            g.DrawString(item.title, e.Font, item.answer > 0 ? Brushes.Green : Brushes.Black, new PointF(e.Bounds.X, e.Bounds.Y));
+            e.DrawFocusRectangle();
         }
     }
 }
